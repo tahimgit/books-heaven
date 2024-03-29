7b2992d303afe8ec5c445d79f2a03ebb3998dbd1
tahimgit <alamtahim3@gmail.coom>
2024-03-29 19:59:54 +0600

    update pageToRead page

--- src/pages/PageToRead.jsx
+++ src/pages/PageToRead.jsx
@@ -2,18 +2,6 @@ import { useEffect, useState } from 'react';
 import { LineChart, Line } from 'recharts';
 import { getBooks } from "../utiity";
 const PageToRead = () => {
-    const studentData = [
-        { student: 1, marks: 85 },
-        { student: 2, marks: 72 },
-        { student: 3, marks: 90 },
-        { student: 4, marks: 78 },
-        { student: 5, marks: 95 },
-        { student: 6, marks: 60 },
-        { student: 7, marks: 88 },
-        { student: 8, marks: 79 },
-        { student: 9, marks: 83 },
-        { student: 10, marks: 91 }
-    ]; 
     const [listedBook, setListedBook] = useState([]);
     useEffect(() => {
         const savedBooks = getBooks();
